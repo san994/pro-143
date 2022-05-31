a00c45b849009e8d9decca61e6e0bafd791eae30
san994 <75426313+san994@users.noreply.github.com>
2022-05-30 22:47:44 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './Screens/Home';
+
+export default function App() {
+  return <HomeScreen/>  
+}
